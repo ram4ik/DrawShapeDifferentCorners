97161e830f52b36a84bbc10d319cea8169739dc9
Ramil <ram4ik@hotmail.com>
2022-10-30 21:47:32 +0200

    Initial Commit

---
+++ DrawShapeDifferentCorners/DrawShapeDifferentCornersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawShapeDifferentCornersApp.swift
+//  DrawShapeDifferentCorners
+//
+//  Created by Ramill Ibragimov on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawShapeDifferentCornersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
